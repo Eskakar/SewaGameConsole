30fbc7cc769b71972c1a99df1d2b09a83092aeb7
Eskakar <nabilaqilaputra89@gmail.com>
2025-05-31 18:13:25 +0700

    conndatabase

--- SewaGameConsole/src/model/DatabaseConnection.java
+++ SewaGameConsole/src/model/DatabaseConnection.java
@@ -8,6 +8,31 @@ package model;
  *
  * @author ASUS
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author nabil
+ */
 public class DatabaseConnection {
+    private static Connection conn;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                String url = "jdbc:mysql://localhost:3306/latres_prakpbo";
+                String user = "root";
+                String pass = "";
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conn = DriverManager.getConnection(url, user, pass);
+                System.out.println("Connected to DB");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return conn;
+    }
+    
     
 }
